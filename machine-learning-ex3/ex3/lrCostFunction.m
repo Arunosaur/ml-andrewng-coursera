da98a584f9382be7f43d2e7c527c39d669a3e2e0
Arun Swaminathan Rajagopalan <arun@radarinfo.org>
2017-08-20 22:54:02 -0500

    Excercise 3 complete

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,6 +35,11 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+htheta = sigmoid(X * theta);
+J = 1 / m * sum(-y .* log(htheta) - (1 - y) .* log(1 - htheta)) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
+temp = theta;
+temp(1) = 0;
+grad = 1 / m * (X' * (htheta - y) + lambda * temp);
 
 
 
